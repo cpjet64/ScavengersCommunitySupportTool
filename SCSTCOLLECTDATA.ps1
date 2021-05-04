c7d41eac4c575c71e4d1f9967fcd8bbfd1f31b0e
Roy Ashbrook <rashbrook@patriottrans.com>
2021-05-04 15:58:57 -0400

    gitignore, formatting, hide progress bars

--- SCSTCOLLECTDATA.ps1
+++ SCSTCOLLECTDATA.ps1
@@ -4,51 +4,19 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 # Set location to script path
 Set-Location (Split-Path -Parent $MyInvocation.MyCommand.Path)
 
-##
-#BEGIN VARIABLES
-##
-
+# Settings
 $PSTitle = "Scavengers Community Support Tool Data Only"
-$host.UI.RawUI.WindowTitle = $PSTitle
 $scstlog = "SCSTInfo.log"
+$outfile = @{FilePath=$scstlog;Encoding="utf8";Append=$true}
 
-##
-#END VARIABLES
-##
+# Set Window Title
+$host.UI.RawUI.WindowTitle = $PSTitle
 
-##
-#BEGIN LOGGING This section gathers information about the computer specs and creates and appends the data to the SST logfile.
-##
-
+# Log Data
+$ProgressPreference = "SilentlyContinue" #hide progress bars
 Get-ComputerInfo | Select-Object WindowsProductName, WindowsVersion, OsHardwareAbstractionLayer | Out-File -FilePath "$scstlog" -Encoding utf8 -Force
-Get-CimInstance -ClassName CIM_Processor | Select-Object -Property Name, MaxClockSpeed, SocketDesignation, Manufacturer | Out-File -FilePath "$scstlog" -Encoding utf8 -Append
-Get-CimInstance -ClassName CIM_VideoController | Format-Table -AutoSize -Property Name, CurrentHorizontalResolution, CurrentVerticalResolution, CurrentRefreshRate, @{Name="AdapterRamGB"; Expression={[int]($_.AdapterRam/1GB)}}, DriverDate, DriverVersion | Out-File -FilePath "$scstlog" -Encoding utf8 -Append
-Get-CimInstance -ClassName CIM_PhysicalMemory | Format-Table -AutoSize Manufacturer, PartNumber, Speed, DeviceLocator, @{Name="CapacityGB"; Expression={[int]($_.Capacity/1GB)}} | Out-File -FilePath "$scstlog" -Encoding utf8 -Append
-Get-CimInstance -ClassName CIM_DiskDrive | Format-Table -AutoSize DeviceID, Model, @{Name="SizeGB"; Expression={[int]($_.Size/1GB)}} | Out-File -FilePath "$scstlog" -Encoding utf8 -Append
-Get-CimInstance -ClassName CIM_LogicalDisk | Format-Table -AutoSize DeviceID, @{Name="SizeGB"; Expression={[int]($_.Size/1GB)}} | Out-File -FilePath "$scstlog" -Encoding utf8 -Append
-
-##
-#END LOGGING
-##
-
-##
-#BEGIN LOG TRANSMITTAL Here we are automatically posting the SST.log file to the discord for dev tracking and troubleshooting.
-##
-
-
-
-##
-#END LOG TRANSMITTAL
-##
-
-##
-#BEGIN HOUSEKEEPING Here we are removing all of the files and folders we created during the process so we arent leaving things behind.
-##
-
-#Remove-Item -Path "$env:TEMP\SCST.log"
-
-##
-#END HOUSEKEEPING
-##
-
-$host.UI.RawUI.ReadKey();
+Get-CimInstance -ClassName CIM_Processor | Select-Object -Property Name, MaxClockSpeed, SocketDesignation, Manufacturer | Out-File @outfile
+Get-CimInstance -ClassName CIM_VideoController | Format-Table -AutoSize -Property Name, CurrentHorizontalResolution, CurrentVerticalResolution, CurrentRefreshRate, @{Name="AdapterRamGB"; Expression={[int]($_.AdapterRam/1GB)}}, DriverDate, DriverVersion | Out-File @outfile
+Get-CimInstance -ClassName CIM_PhysicalMemory | Format-Table -AutoSize Manufacturer, PartNumber, Speed, DeviceLocator, @{Name="CapacityGB"; Expression={[int]($_.Capacity/1GB)}} | Out-File @outfile
+Get-CimInstance -ClassName CIM_DiskDrive | Format-Table -AutoSize DeviceID, Model, @{Name="SizeGB"; Expression={[int]($_.Size/1GB)}} | Out-File @outfile
+Get-CimInstance -ClassName CIM_LogicalDisk | Format-Table -AutoSize DeviceID, @{Name="SizeGB"; Expression={[int]($_.Size/1GB)}} | Out-File @outfile
