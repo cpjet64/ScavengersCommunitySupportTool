a1151125eb4dc109012b2a04a6729065f45f782c
Roy Ashbrook <rashbrook@patriottrans.com>
2021-05-04 15:41:25 -0400

    formatting changes

--- SCSTCOLLECTDATA.ps1
+++ SCSTCOLLECTDATA.ps1
@@ -1,7 +1,5 @@
+# Run a Powershell script that isnt signed ONCE and ONLY DURING THIS SESSION
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-
-#set location to script path
-Set-Location (Split-Path -Parent $MyInvocation.MyCommand.Path)
 
 ###
 #BEGIN SELF ELEVATE TO ADMIN
@@ -20,19 +18,15 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 #END SELF ELEVATE TO ADMIN
 ###
 
-
-##
-#The section above allows us to run a Powershell script that isnt signed ONCE and ONLY DURING THIS SESSION
-##
+# Set location to script path
+Set-Location (Split-Path -Parent $MyInvocation.MyCommand.Path)
 
 ##
 #BEGIN VARIABLES
 ##
 
 $PSTitle = "Scavengers Community Support Tool Data Only"
-
 $host.UI.RawUI.WindowTitle = $PSTitle
-
 $scstlog = "SCSTInfo.log"
 
 ##
@@ -44,15 +38,10 @@ $scstlog = "SCSTInfo.log"
 ##
 
 Get-ComputerInfo | Select-Object WindowsProductName, WindowsVersion, OsHardwareAbstractionLayer | Out-File -FilePath "$scstlog" -Encoding utf8 -Force
-
 Get-CimInstance -ClassName CIM_Processor | Select-Object -Property Name, MaxClockSpeed, SocketDesignation, Manufacturer | Out-File -FilePath "$scstlog" -Encoding utf8 -Append
-
 Get-CimInstance -ClassName CIM_VideoController | Format-Table -AutoSize -Property Name, CurrentHorizontalResolution, CurrentVerticalResolution, CurrentRefreshRate, @{Name="AdapterRamGB"; Expression={[int]($_.AdapterRam/1GB)}}, DriverDate, DriverVersion | Out-File -FilePath "$scstlog" -Encoding utf8 -Append
-
 Get-CimInstance -ClassName CIM_PhysicalMemory | Format-Table -AutoSize Manufacturer, PartNumber, Speed, DeviceLocator, @{Name="CapacityGB"; Expression={[int]($_.Capacity/1GB)}} | Out-File -FilePath "$scstlog" -Encoding utf8 -Append
-
 Get-CimInstance -ClassName CIM_DiskDrive | Format-Table -AutoSize DeviceID, Model, @{Name="SizeGB"; Expression={[int]($_.Size/1GB)}} | Out-File -FilePath "$scstlog" -Encoding utf8 -Append
-
 Get-CimInstance -ClassName CIM_LogicalDisk | Format-Table -AutoSize DeviceID, @{Name="SizeGB"; Expression={[int]($_.Size/1GB)}} | Out-File -FilePath "$scstlog" -Encoding utf8 -Append
 
 ##
