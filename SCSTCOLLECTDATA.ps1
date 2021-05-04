bfe42c97a6b6973a5a832b24ab2c41730a3ec2df
Roy Ashbrook <rashbrook@patriottrans.com>
2021-05-04 16:11:46 -0400

    add upload code

--- SCSTCOLLECTDATA.ps1
+++ SCSTCOLLECTDATA.ps1
@@ -6,13 +6,17 @@ Set-Location (Split-Path -Parent $MyInvocation.MyCommand.Path)
 
 # Includes
 . .\New-ScstLog.ps1
+. .\Submit-TextFile.ps1
 
 # Settings
-$PSTitle = "Scavengers Community Support Tool Data Only"
-$scstlog = "SCSTInfo.log"
+$pst = "Scavengers Community Support Tool Data Only"
+$log = "SCSTInfo.log"
+$uri = 'https://discord.com/api/webhooks/838597970369708123/f95KX4IkB4qbt10eMdiW9SMyBEf_5uMPsr2vwXtoxNmE6OG4B0fFGM_KUesDb8wOTEJQ'
 
 # Set Window Title
-$host.UI.RawUI.WindowTitle = $PSTitle
+$host.UI.RawUI.WindowTitle = $pst
 
-# Generate Log
-New-ScstLog $scstlog
+# Generate Log and send to Discord
+if(New-ScstLog $log){
+  $null = Submit-TextFile $log $uri
+}
