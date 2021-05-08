d900c74cf162f2291f9c960498732f8de77f1591
Curt <curt.petricca@yahoo.com>
2021-05-07 23:13:08 -0400

    added filehashes

--- getfilehash.ps1
+++ getfilehash.ps1
@@ -1,19 +1,20 @@
-$DATETIME = Get-Date
-$TIMEZONE = Get-TimeZone
+$DATETIME = Get-Date -Format FileDateTimeUniversal
+$FILENAME = ".\FileHashes.txt"
 
-New-Item ".\FILEHASH.txt" -Force
+New-Item .\FileHashes.txt -Force
 
-Add-Content -Path ".\FILEHASH.txt" -Value "`r`nThis Hash file was generated on $DATETIME $TIMEZONE by Cpjet64" -Encoding UTF8
+Add-Content -Path "$FILENAME" -Value "`r`nThis Hash file was generated on $DATETIME by Cpjet64" -Encoding UTF8
 
-Get-FileHash .\compiled\ScavengersCST.exe -Algorithm MD5 | Format-Table -AutoSize -Property Algorithm, Hash | Out-File FILEHASH.txt -Append -Encoding utf8
+Get-FileHash .\compiled\ScavengersCST.exe -Algorithm MD5 | Format-Table -AutoSize -Property Algorithm, Hash | Out-File $FILENAME -Append -Encoding utf8
 
-Get-FileHash .\compiled\ScavengersCST.exe -Algorithm SHA1 | Format-Table -AutoSize -Property Algorithm, Hash | Out-File FILEHASH.txt -Append -Encoding utf8
+Get-FileHash .\compiled\ScavengersCST.exe -Algorithm SHA1 | Format-Table -AutoSize -Property Algorithm, Hash | Out-File $FILENAME -Append -Encoding utf8
 
-Get-FileHash .\compiled\ScavengersCST.exe -Algorithm SHA256 | Format-Table -AutoSize -Property Algorithm, Hash | Out-File FILEHASH.txt -Append -Encoding utf8
+Get-FileHash .\compiled\ScavengersCST.exe -Algorithm SHA256 | Format-Table -AutoSize -Property Algorithm, Hash | Out-File $FILENAME -Append -Encoding utf8
 
-Get-FileHash .\compiled\ScavengersCST.exe -Algorithm SHA512 | Format-Table -AutoSize -Property Algorithm, Hash | Out-File FILEHASH.txt -Append -Encoding utf8
+Get-FileHash .\compiled\ScavengersCST.exe -Algorithm SHA512 | Format-Table -AutoSize -Property Algorithm, Hash | Out-File $FILENAME -Append -Encoding utf8
 
-Get-FileHash .\compiled\ScavengersCST.exe -Algorithm MACTripleDES | Format-Table -AutoSize -Property Algorithm, Hash | Out-File FILEHASH.txt -Append -Encoding utf8
+Get-FileHash .\compiled\ScavengersCST.exe -Algorithm MACTripleDES | Format-Table -AutoSize -Property Algorithm, Hash | Out-File $FILENAME -Append -Encoding utf8
 
-Get-FileHash .\compiled\ScavengersCST.exe -Algorithm RIPEMD160 | Format-Table -AutoSize -Property Algorithm, Hash | Out-File FILEHASH.txt -Append -Encoding utf8
+Get-FileHash .\compiled\ScavengersCST.exe -Algorithm RIPEMD160 | Format-Table -AutoSize -Property Algorithm, Hash | Out-File $FILENAME -Append -Encoding utf8
 
+Rename-Item -Path .\"FileHashes.txt" -NewName .\"FileHashes$DATETIME.txt"
